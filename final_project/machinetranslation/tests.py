cff04c30664d649b1b0d59bf978560898cc435f1
debdut-roy <devdutray@gmail.com>
2022-12-29 08:21:07 -0500

    Updated unit tests

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,21 +1,21 @@
 import unittest
 
-from translator import englishToFrench, frenchToEnglish
+from translator import english_to_french, french_to_english
 
 class TestEnglishToFrech(unittest.TestCase): 
 
     def test_english_to_french_null(self):
         # If null input then translation = null
-        self.assertIsNone(englishToFrench(None),None)
+        self.assertIsNone(english_to_french(None),None)
 
     def test_english_to_french(self):
         # If 'Hello' input then translation = 'Bonjour'
-        self.assertEqual(englishToFrench('Hello'),'Bonjour')
+        self.assertEqual(english_to_french('Hello'),'Bonjour')
     
     def test_french_to_english__null(self):
         # If null input then translation = null
-        self.assertIsNone(frenchToEnglish(None),None)
+        self.assertIsNone(french_to_english(None),None)
 
     def test_french_to_english(self):
         # If 'Hello' input then translation = 'Bonjour'
-        self.assertEqual(frenchToEnglish('Bonjour'),'Hello')
+        self.assertEqual(french_to_english('Bonjour'),'Hello')
