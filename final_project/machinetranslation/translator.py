df72116770708eabb2b44ce41b41f4c7d1edc0fa
debdut-roy <devdutray@gmail.com>
2022-12-29 02:24:03 -0500

    Updated translator wrt pylink

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,8 +1,14 @@
+"""
+English-French Translator using IBM Watson
+This module can be used to translate text from English to French and vice-versa
+"""
+
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -17,18 +23,20 @@ language_translator = LanguageTranslatorV3(
 language_translator.set_service_url(url)
 # language_translator.set_disable_ssl_verification(True)
 
-def englishToFrench(english_text):
+def english_to_french(english_text):
     """
     This function translates English to French
     """
-    french_translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    french_translation = language_translator.translate(text=english_text,
+    model_id='en-fr').get_result()
     french_text = french_translation.get('translations')[0].get('translation')
     return french_text
 
-def frenchToEnglish(french_text):
+def french_to_english(french_text):
     """
     This function translates French to English
     """
-    english_translation = language_translator.translate(text=french_text, model_id='fr-en').get_result()
+    english_translation = language_translator.translate(text=french_text,
+    model_id='fr-en').get_result()
     english_text = english_translation.get('translations')[0].get('translation')
     return english_text
